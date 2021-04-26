dcee24556370ec2c25fef9d46c3c18bc9b80f9ff
poleumdev <64774219+poleumdev@users.noreply.github.com>
2021-04-26 09:52:32 +0200

    Update version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_history_attestoodle';
 $plugin->release = '1.0.0';
-$plugin->version = 2021031201;
+$plugin->version = 2021042601;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
